7e137a36c4b846e5492184f39047de16ceb339e3
UWStat302 Student <uwstat302student@gmail.com>
2020-10-23 20:03:04 -0700

    adding my_iris data

---
+++ R/my_iris.r
@@ -0,0 +1,20 @@
+#' My Iris dataset
+#'
+#' This data based on iris to be used as example.
+#'
+#'@describe{
+#'\item{Sepal.Lenghth}{sepal length, in cm}
+#'\item{Sepal.Width}{sepal width, in cm}
+#'\item{Petal.Length}{petal lenghth, cm}
+#'\item{Petal.Width}{petal width, in cm}
+#'\item{Species}{spacies of iris}
+#'}
+#'
+#'@source
+#'Fisher R.A. (1936) he use of multiple measurements in taxonomic problems.
+#'\emph{Annals of Eugenics} 7, Part II, 179–188.
+#'The data were collected by Anderson, Edgar (1935).
+#'The irises of the Gaspe Peninsula, \emph{Bulletin of the American Iris Society}, 59, 2–5.
+#'
+
+"my_iris"
